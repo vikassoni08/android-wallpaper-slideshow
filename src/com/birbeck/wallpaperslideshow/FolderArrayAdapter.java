2b767dd1a6a842113c579b50b4b94f6262269a75
birbeck <birbeck@e0fea54c-78fb-bbf5-d915-d57ffccd4782>
2011-05-16 05:27:38 +0000

    Merged with latest Android Market version Added more durations Added manual duation (tap to change only) Added option to change wallpaper on screen awake Added launcher activity with shortcut to set wallpaper and settings Preferences changes no longer trigger wallpaper change except for changing folder

---
+++ src/com/birbeck/wallpaperslideshow/FolderArrayAdapter.java
@@ -0,0 +1,100 @@
+/*
+ *    Copyright (C) 2010 Stewart Gateley <birbeck@gmail.com>
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.birbeck.wallpaperslideshow;
+
+import java.io.File;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class FolderArrayAdapter extends ArrayAdapter<String> {
+	
+	private static boolean mHasThumbnailUtils;
+	static {
+		try {
+			ThumbnailUtilsWrapper.checkAvailable();
+			mHasThumbnailUtils = true;
+		} catch (Throwable t) {
+			mHasThumbnailUtils = false;
+		}
+	}
+
+	private final LayoutInflater mInflater;
+	private final String[] mFolders;
+
+	public FolderArrayAdapter(Context context, int layout,
+			String[] folders) {
+		super(context, layout, folders);
+		this.mInflater = LayoutInflater.from(context);
+		this.mFolders = folders;
+	}
+
+	@Override
+	public View getView(int position, View view, ViewGroup parent) {
+		if (view == null) {
+			view = mInflater.inflate(R.layout.select_folder_list_item, null);
+			new CachedView(view);
+		}
+
+		CachedView cache = (CachedView)view.getTag();
+
+		String folder = mFolders[position];
+		if (folder != null) {
+			cache.image.setImageResource(android.R.drawable.ic_menu_gallery);
+			cache.text1.setText(new File(folder).getName());
+			cache.text2.setText(folder);
+			try {
+				File[] images = new File(folder).listFiles(SelectFolderActivity.ImageFilter);
+				if (images.length > 0) {
+					cache.text1.setText(new File(folder).getName() + " (" + images.length + ")");
+					Bitmap bitmap = BitmapUtil.makeBitmap(75, 10000,
+							images[0].getAbsolutePath(), null);
+					if (mHasThumbnailUtils) {
+						bitmap = ThumbnailUtilsWrapper.extractThumbnail(
+								bitmap, 75, 75);
+					} else {
+						bitmap = BitmapUtil.transform(null, bitmap, 75, 75, false, true);
+					}
+					cache.image.setImageBitmap(bitmap);
+				}
+			} catch (Exception e) { }
+		}
+
+		return view;
+	}
+
+	private class CachedView {
+		public ImageView image;
+		public TextView text1;
+		public TextView text2;
+
+		public CachedView(View v) {
+			image = (ImageView) v.findViewById(R.id.image);
+			text1 = (TextView) v.findViewById(R.id.text1);
+			text2 = (TextView) v.findViewById(R.id.text2);
+			v.setTag(this);
+		}
+	};
+	
+}
